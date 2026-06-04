0f1070b431ee72876cdd4b94ab0b6310f8f1213a
ddaaniel <daniel0333v@gmail.com>
2026-06-04 12:08:09 -0300

    dealing with low-level abstractoins and implementations and playing with references and pointers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 mod type_view;
 
-use std::time::{SystemTime, UNIX_EPOCH};
-
+use std::{
+    mem::MaybeUninit,
+    time::{Instant, SystemTime, UNIX_EPOCH},
+};
 use type_view::type_overview;
+
+const CLOCK_REALTIME_COARSE: i32 = 5;
+
+unsafe extern "C" {
+    fn clock_gettime(clock_id: i32, tp: *mut Timespec) -> i32;
+}
+
+#[repr(C)]
+struct Timespec {
+    tv_sec: i64,
+    tv_nsec: i64,
+}
 
 fn main() {
     type_overview();
@@ -17,14 +31,68 @@ fn main() {
     }
 
     dosmt(&var);
+    unix_epoch();
+    coarse_clock();
+    libc_coarse_clock();
+}
 
-    let unx_epoch = SystemTime::now()
+
+fn coarse_clock() {
+    let init = Instant::now();
+
+    let unix_epoch = unsafe {
+        let mut t = MaybeUninit::<Timespec>::uninit();
+        clock_gettime(CLOCK_REALTIME_COARSE, t.as_mut_ptr());
+        t.assume_init().tv_sec
+    };
+
+    let secs = (unix_epoch % 86400) as u32;
+    let h = (secs / 3600) as i32;
+    let m = (secs % 3600) / 60;
+    let utc_hour = (h + 24 - 3) % 24;
+    println!(
+        "coarse clock_time -> {:02}:{:02} in {:?}",
+        utc_hour,
+        m,
+        init.elapsed()
+    );
+}
+
+fn libc_coarse_clock() {
+    let init = Instant::now();
+
+    let mut t = MaybeUninit::<libc::timespec>::uninit();
+    let unix_epoch = unsafe {
+        libc::clock_gettime(libc::CLOCK_REALTIME_COARSE, t.as_mut_ptr());
+        t.assume_init().tv_sec
+    };
+    let secs = (unix_epoch % 86400) as u32;
+    let h = (secs / 3600) as i32;
+    let m = (secs % 3600) / 60;
+    let utc_hour = (h + 24 - 3) % 24;
+    println!(
+        "libc clock_time -> {:02}:{:02} in {:?}",
+        utc_hour,
+        m,
+        init.elapsed()
+    );
+}
+
+fn unix_epoch() {
+    let init = Instant::now();
+
+    let unix_epoch = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_secs();
-    let secs = (unx_epoch % 86400) as u32;
+    let secs = (unix_epoch % 86400) as u32;
     let h = (secs / 3600) as i32;
-    let m =  (secs % 3600) / 60;
+    let m = (secs % 3600) / 60;
     let utc_hour = (h + 24 - 3) % 24;
-    println!("clock_time -> {:02}:{:02}", utc_hour, m);
+    println!(
+        "timeSystem clock_time -> {:02}:{:02} in {:?}",
+        utc_hour,
+        m,
+        init.elapsed()
+    );
 }
